639c2b48fee9780be92fb566098a1fd83c9ee5ce
timothy <timothy.green@ecstech.com>
2023-01-27 18:10:31 -0700

    Finishing up

--- src/App.tsx
+++ src/App.tsx
@@ -8,16 +8,12 @@ interface Quote {
 }
 
 function App() {
-  const [count, setCount] = useState(0)
   const [name, setName] = useState('');
   const [quotes, setQuotes] = useState<Quote[]>([]);
-
-  const [randomQuote, setRandomQuote] = useState<Quote | null>(null);
 
   async function loadRandomQuote() {
     const result = await fetch("https://usu-quotes-mimic.vercel.app/api/random");
     const quote = await result.json();
-    console.log(quote)
     setQuotes([quote]);
   } 
 
@@ -27,31 +23,24 @@ function App() {
 
   async function submitRequest(event: FormEvent<HTMLFormElement>) {
     event.preventDefault()
-    console.log(name)
     const search = "https://usu-quotes-mimic.vercel.app/api/search?query=" + name;
-    console.log(search)
-    const result2 = await fetch(search);
-    const resultsQuotes = await result2.json();
+    const result = await fetch(search);
+    const resultsQuotes = await result.json();
     setQuotes(resultsQuotes.results)
-    console.log(resultsQuotes.results);
   }
 
   return (
 
     <div>
-
       <div className='title stuff-box'>
         <h1>Quotable</h1>
         <h3>Search Notable Quotes by Author</h3>
       </div>
-
       <div>
         <form onSubmit={e => submitRequest(e)}>
           <input type="text" value={name} placeholder='Benjamin Franklin' onChange={e => setName(e.target.value)}/>
         </form>
       </div>
-
-      
       <div>
         {
           quotes.map((quote) => (
@@ -62,7 +51,6 @@ function App() {
           ))
         }
       </div>
-
     </div>
   )
 }
